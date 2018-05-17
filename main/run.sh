40adeda956e13e65e85b8abd6b240e044b39e2e1
Ilya <rognarock@gmail.com>
2018-05-17 14:50:47 +0300

    add correct path to key

--- main/run.sh
+++ main/run.sh
@@ -2,7 +2,7 @@
 echo 192.168.56.62 hello-world.com kibana.hello-world.com jenkins.hello-world.com >> /etc/hosts
 vagrant up
 sleep 2m
-ssh -i /home/rogachev/.ssh/keys/virtualbox/server_ca vagrant@192.168.56.44 << EOF
+ssh -i ./templates/server_ca vagrant@192.168.56.44 << EOF
 sudo /opt/puppetlabs/bin/puppet cert sign --all
 sudo puppet module install rtyler/jenkins
 sudo puppet module install rehan-nginx
