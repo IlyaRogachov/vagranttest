ec7be56b781c585aae36a9780481f7257d834954
IlyaRogachov <rognarock@gmail.com>
2018-05-17 18:45:40 +0300

    Update run.sh

--- additional/run.sh
+++ additional/run.sh
@@ -4,7 +4,7 @@ vagrant up
 sleep 1m
 cd ./own && ansible-playbook -i inventory.cfg playbook/ownrun.yaml
 sleep 3m
-cd ../kargo && ansible-playbook -i inventory/inventory.cfg cluster.yml
+cd ../kargo_bk && ansible-playbook -i inventory/inventory.cfg cluster.yml
 sleep 1m
 cd ../dockerrepo && ansible-playbook -i inventory.cfg playbook/ownrun.yaml
 sleep 1m
